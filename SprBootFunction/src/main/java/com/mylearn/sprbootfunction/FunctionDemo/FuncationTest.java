9387507a436578f111819bc6c238dc631c860a1d
qilianggao <gaoqiliang@vv.cn>
2022-05-23 16:34:58 +0800

    Function测试

---
+++ SprBootFunction/src/main/java/com/mylearn/sprbootfunction/FunctionDemo/FuncationTest.java
@@ -0,0 +1,13 @@
+package com.mylearn.sprbootfunction.FunctionDemo;
+
+/**
+ * java8 funcation 测试
+ *
+ * 函数式接口主要分为
+ * Supplier供给型函数  【不接受参数、只返回数据】
+ * Consumer消费型函数   【】
+ * Runnable无参无返回型函数 【】
+ * Function有参有返回型函数  【】
+ */
+public class FuncationTest {
+}
