d15e6e18d1c4f251cbf68c1d643356a9ea24d8bf
steve_gao <gaoqiliang1104@163.com>
2022-08-17 18:18:03 +0800

    ratelimit

---
+++ RateLimitGuava/src/main/java/com/example/ratelimitguava/RateLimitGuavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratelimitguava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitGuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimitGuavaApplication.class, args);
+    }
+
+}
---
+++ RateLimitGuava/src/test/java/com/example/ratelimitguava/RateLimitGuavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratelimitguava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitGuavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
