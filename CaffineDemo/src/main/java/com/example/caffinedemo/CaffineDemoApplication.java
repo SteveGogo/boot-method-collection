29ee7f67556d3537de02fe3e5f1b68c97344a6fe
steve_gao <gaoqiliang1104@163.com>
2022-08-17 18:28:04 +0800

    caffeine cache

---
+++ CaffineDemo/src/main/java/com/example/caffinedemo/CaffineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.caffinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffineDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaffineDemoApplication.class, args);
+    }
+
+}
