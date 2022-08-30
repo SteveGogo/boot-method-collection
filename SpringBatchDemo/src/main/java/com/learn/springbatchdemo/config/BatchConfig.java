c586f453a6d3460361c9ae3eec3cd574a09b534d
steve_gao <gaoqiliang1104@163.com>
2022-08-31 00:02:05 +0800

    springb-batch-demo

--- SpringBatchDemo/src/main/java/com/learn/springbatchdemo/config/BatchConfig.java
+++ SpringBatchDemo/src/main/java/com/learn/springbatchdemo/config/BatchConfig.java
@@ -1,27 +1,49 @@
 package com.learn.springbatchdemo.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing
+import javax.annotation.Resource;
+
 @Configuration
+@EnableBatchProcessing
 public class BatchConfig {
 
-    //创建任务的工厂[job由step组成]
-    @Autowired
+    @Resource
     private JobBuilderFactory jobBuilderFactory;
 
-    //创建步骤的工厂
-    @Autowired
+    @Resource
     private StepBuilderFactory stepBuilderFactory;
 
 
+    @Bean
+    public Job helloWorld(){
+        return jobBuilderFactory.get("hello-world-job")
+                .start(step1())
+                .build();
+    }
 
+    @Bean
+    public  Step step1() {
 
+        return stepBuilderFactory.get("step1").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("hello world step1");
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
 
 
 }
