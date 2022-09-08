79dab20a1630425738f1fbd827dc4cf748f59fae
steve_gao <gaoqiliang1104@163.com>
2022-09-08 22:40:08 +0800

    log-demo

---
+++ MINAdemo/src/main/java/com/example/minademo/MinAdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.minademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinAdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinAdemoApplication.class, args);
+    }
+
+}
