65b27ed396bb345461e5ec796e181c676c70139e
steve_gao <gaoqiliang1104@163.com>
2022-09-09 10:01:10 +0800

    log-demo

--- LogbackDemo/src/test/java/com/example/logbackdemo/LogbackDemoApplicationTests.java
+++ LogbackDemo/src/test/java/com/example/logbackdemo/LogbackDemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.logbackdemo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,18 @@ class LogbackDemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+    @Test
+    void contextLoads1() {
+        //日志的级别从低到高
+        logger.trace("这是trace日志。。。");
+        logger.debug("这是dubug日志。。。");
+        logger.info("这是info日志。。。");
+        logger.warn("这是warn日志。。。");
+        logger.error("这是error日志");
     }
 
 }
