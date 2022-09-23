8f3e2422c28f362ce70b15405db688c092477ce3
steve_gao <gaoqiliang1104@163.com>
2022-09-23 09:50:49 +0800

    sa-token

---
+++ JustAuthDemo/src/main/java/com/example/justauthdemo/JustAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.justauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAuthDemoApplication.class, args);
+	}
+
+}
