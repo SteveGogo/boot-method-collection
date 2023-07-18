f37a0e432450534360a2a2d70e9c3e29c10c974f
gaoqiliang <gaoqiliang1104@163.com>
2023-07-18 13:28:26 +0800

    update

--- SprBootFunction/src/main/java/com/mylearn/sprbootfunction/Time/TimeTest.java
+++ SprBootFunction/src/main/java/com/mylearn/sprbootfunction/Time/TimeTest.java
@@ -1,13 +1,25 @@
 package com.mylearn.sprbootfunction.Time;
 
+import cn.hutool.core.date.LocalDateTimeUtil;
+
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class TimeTest {
+//    public static void main(String[] args) {
+//
+//        LocalDateTime now = LocalDateTime.now();
+//
+//        System.out.println(now.toLocalDate());
+//        System.out.println(now);
+//    }
+
     public static void main(String[] args) {
-
         LocalDateTime now = LocalDateTime.now();
-
-        System.out.println(now.toLocalDate());
-        System.out.println(now);
+        LocalDateTime next = now.plusHours(1);
+        int between = (int) LocalDateTimeUtil.between(LocalDateTime.now(), next, ChronoUnit.SECONDS);
+        System.out.println(between);
     }
+
+
 }
